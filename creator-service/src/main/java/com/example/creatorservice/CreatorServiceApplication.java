b5362db5b77b7e66a59731085080f1e6aa4f6610
corinB <gkdisrha2020@gmail.com>
2026-03-23 09:42:41 +0900

    hello msa

---
+++ creator-service/src/main/java/com/example/creatorservice/CreatorServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.creatorservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreatorServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreatorServiceApplication.class, args);
+	}
+
+}
