8e201afa2a01aec9ac086b263bba665fcd16542e
leeejunu <leeejunu@gmail.com>
2026-03-24 14:08:37 +0900

    feat(user): Permission 레포지토리 추가

---
+++ user-service/src/main/java/com/example/userservice/infrastructure/PermissionRepositoryAdapter.java
@@ -0,0 +1,21 @@
+package com.example.userservice.infrastructure;
+
+import com.example.userservice.domain.model.Permission;
+import com.example.userservice.domain.model.Role;
+import com.example.userservice.domain.repository.PermissionRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class PermissionRepositoryAdapter implements PermissionRepository {
+
+    private final PermissionJpaRepository permissionJpaRepository;
+
+    @Override
+    public List<Permission> findByRole(Role role) {
+        return permissionJpaRepository.findByRole(role);
+    }
+}
