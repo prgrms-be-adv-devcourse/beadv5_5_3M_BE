ea6c9122768ff01e5fb2350698e869b4f33d290d
JunHeeCh <apdlsid1@naver.com>
2026-03-25 22:20:44 +0900

    feat(movie): 영화 정보 수정 로직 구현

--- movie-service/src/main/java/com/example/movieservice/application/usecase/MovieUseCase.java
+++ movie-service/src/main/java/com/example/movieservice/application/usecase/MovieUseCase.java
@@ -1,6 +1,7 @@
 package com.example.movieservice.application.usecase;
 
 import com.example.movieservice.presentation.dto.request.RegisterMovieRequest;
+import com.example.movieservice.presentation.dto.request.UpdateDetailRequest;
 import com.example.movieservice.presentation.dto.request.UpdateVisibilityRequest;
 import com.example.movieservice.presentation.dto.response.RegisterMovieResponse;
 
@@ -10,4 +11,6 @@ public interface MovieUseCase {
     RegisterMovieResponse register(UUID creatorId, RegisterMovieRequest request);
 
     void updateVisibility(UUID creatorId, Long movieId, UpdateVisibilityRequest request);
+
+    void updateDetail(UUID creatorId, Long movieId, UpdateDetailRequest request);
 }
