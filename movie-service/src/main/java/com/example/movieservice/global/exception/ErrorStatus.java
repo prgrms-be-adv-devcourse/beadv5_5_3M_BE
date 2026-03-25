61f54ef9a1ab5199cb9dfb2ae698dbf4f8cfa102
JunHeeCh <apdlsid1@naver.com>
2026-03-25 21:53:24 +0900

    feat(movie): 커스텀 에러 추가

--- movie-service/src/main/java/com/example/movieservice/global/exception/ErrorStatus.java
+++ movie-service/src/main/java/com/example/movieservice/global/exception/ErrorStatus.java
@@ -9,14 +9,18 @@ import org.springframework.http.HttpStatus;
 public enum ErrorStatus implements BaseErrorCode {
 
     // Common
-    _INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "COMMON500", "서버 에러가 발생했습니다"),
-    _BAD_REQUEST(HttpStatus.BAD_REQUEST, "COMMON400", "잘못된 요청입니다"),
-    _UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "COMMON401", "인증이 필요합니다"),
-    _FORBIDDEN(HttpStatus.FORBIDDEN, "COMMON403", "접근 권한이 없습니다"),
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "COMMON500", "서버 에러가 발생했습니다"),
+    BAD_REQUEST(HttpStatus.BAD_REQUEST, "COMMON400", "잘못된 요청입니다"),
+    UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "COMMON401", "인증이 필요합니다"),
+    FORBIDDEN(HttpStatus.FORBIDDEN, "COMMON403", "접근 권한이 없습니다"),
 
-    // Creator
-    CREATOR_DUPLICATE_PHONE(HttpStatus.CONFLICT, "CREATOR409", "이미 등록된 전화번호입니다"),
-    CREATOR_NOT_FOUND(HttpStatus.NOT_FOUND, "CREATOR404", "크리에이터를 찾을 수 없습니다");
+    // Movie
+    MOVIE_REGISTRATION_LIMIT_EXCEEDED(HttpStatus.BAD_REQUEST, "MOVIE400", "영화는 최대 3개까지 등록할 수 있습니다"),
+    MOVIE_NOT_FOUND(HttpStatus.NOT_FOUND, "MOVIE404", "영화를 찾을 수 없습니다"),
+    MOVIE_INVALID_CREATOR(HttpStatus.FORBIDDEN, "MOVIE403", "해당 영화에 대한 권한이 없습니다"),
+
+    // Category
+    CATEGORY_NOT_FOUND(HttpStatus.NOT_FOUND, "CATEGORY404", "카테고리를 찾을 수 없습니다");
 
     private final HttpStatus httpStatus;
     private final String code;
