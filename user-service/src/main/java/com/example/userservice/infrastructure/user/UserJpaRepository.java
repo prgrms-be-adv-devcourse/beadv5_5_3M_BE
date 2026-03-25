1cf148fc94e086bbe6a7ed06b256043ef590d058
leeejunu <leeejunu@gmail.com>
2026-03-25 12:22:48 +0900

    chore(user): user, permission 레포지토리 폴더 분리

--- user-service/src/main/java/com/example/userservice/infrastructure/user/UserJpaRepository.java
+++ user-service/src/main/java/com/example/userservice/infrastructure/user/UserJpaRepository.java
@@ -1,4 +1,4 @@
-package com.example.userservice.infrastructure;
+package com.example.userservice.infrastructure.user;
 
 import com.example.userservice.domain.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.UUID;
 
 public interface UserJpaRepository extends JpaRepository<User, UUID> {
+    java.util.Optional<User> findByEmail(String email);
     boolean existsByEmail(String email);
     boolean existsByNickname(String nickname);
 }
