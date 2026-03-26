2dbe401dfc89b92d89fdf05110c5c4ddeb253795
JunHeeCh <apdlsid1@naver.com>
2026-03-26 12:26:43 +0900

    feat(movie): 에러 코드 형식 변경

--- movie-service/src/main/java/com/example/movieservice/global/exception/ErrorStatus.java
+++ movie-service/src/main/java/com/example/movieservice/global/exception/ErrorStatus.java
@@ -9,18 +9,19 @@ import org.springframework.http.HttpStatus;
 public enum ErrorStatus implements BaseErrorCode {
 
     // Common
-    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "COMMON500", "서버 에러가 발생했습니다"),
-    BAD_REQUEST(HttpStatus.BAD_REQUEST, "COMMON400", "잘못된 요청입니다"),
-    UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "COMMON401", "인증이 필요합니다"),
-    FORBIDDEN(HttpStatus.FORBIDDEN, "COMMON403", "접근 권한이 없습니다"),
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "COMMON_INTERNAL_SERVER_ERROR", "서버 에러가 발생했습니다"),
+    BAD_REQUEST(HttpStatus.BAD_REQUEST, "COMMON_BAD_REQUEST", "잘못된 요청입니다"),
+    UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "COMMON_UNAUTHORIZED", "인증이 필요합니다"),
+    FORBIDDEN(HttpStatus.FORBIDDEN, "COMMON_FORBIDDEN", "접근 권한이 없습니다"),
 
     // Movie
-    MOVIE_REGISTRATION_LIMIT_EXCEEDED(HttpStatus.BAD_REQUEST, "MOVIE400", "영화는 최대 3개까지 등록할 수 있습니다"),
-    MOVIE_NOT_FOUND(HttpStatus.NOT_FOUND, "MOVIE404", "영화를 찾을 수 없습니다"),
-    MOVIE_INVALID_CREATOR(HttpStatus.FORBIDDEN, "MOVIE403", "해당 영화에 대한 권한이 없습니다"),
+    MOVIE_REGISTRATION_LIMIT_EXCEEDED(HttpStatus.BAD_REQUEST, "MOVIE_REGISTRATION_LIMIT_EXCEEDED", "영화는 최대 3개까지 등록할 수 있습니다"),
+    MOVIE_NOT_FOUND(HttpStatus.NOT_FOUND, "MOVIE_NOT_FOUND", "영화를 찾을 수 없습니다"),
+    MOVIE_INVALID_CREATOR(HttpStatus.FORBIDDEN, "MOVIE_INVALID_CREATOR", "해당 영화에 대한 권한이 없습니다"),
+    MOVIE_NOT_PUBLIC(HttpStatus.NOT_FOUND, "MOVIE_NOT_PUBLIC", "비공개 영화입니다"),
 
     // Category
-    CATEGORY_NOT_FOUND(HttpStatus.NOT_FOUND, "CATEGORY404", "카테고리를 찾을 수 없습니다");
+    CATEGORY_NOT_FOUND(HttpStatus.NOT_FOUND, "CATEGORY_NOT_FOUND", "카테고리를 찾을 수 없습니다");
 
     private final HttpStatus httpStatus;
     private final String code;
