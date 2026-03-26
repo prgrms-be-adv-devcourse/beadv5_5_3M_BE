ad07a739ca71f7c20ba979d460674343627f62d1
leeejunu <leeejunu@gmail.com>
2026-03-26 15:32:30 +0900

    feat(user): 프로필 수정 기능 추가

--- user-service/src/main/java/com/example/userservice/UserServiceApplication.java
+++ user-service/src/main/java/com/example/userservice/UserServiceApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 
 @SpringBootApplication
-@EnableRedisRepositories(basePackages = "com.example.userservice.infrastructure.redis")
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
---
+++ user-service/src/main/java/com/example/userservice/presentation/dto/res/UserInfoResponse.java
@@ -0,0 +1,9 @@
+package com.example.userservice.presentation.dto.res;
+
+public record UserInfoResponse(
+        String nickname,
+        Integer cookieBalance,
+        String email,
+        String profileUrl
+) {
+}
