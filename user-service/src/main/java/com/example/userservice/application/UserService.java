f6b3aac66676db6d4b348ad0196f0b4d2217dba1
leeejunu <leeejunu@gmail.com>
2026-03-26 12:28:10 +0900

    refactor(user): checkAuthorization 검증 로직 수정

--- user-service/src/main/java/com/example/userservice/application/UserService.java
+++ user-service/src/main/java/com/example/userservice/application/UserService.java
@@ -25,7 +25,7 @@ public class UserService implements UserUseCase {
 
         return permissions.stream()
                 .filter(p -> p.getHttpMethod() == null || p.getHttpMethod().equals(request.httpMethod().name()))
-                .anyMatch(p -> request.requestPath().startsWith(p.getPathPattern()));
+                .noneMatch(p -> request.requestPath().startsWith(p.getPathPattern()));
     }
 
     private UUID toUUID(String userId) {
