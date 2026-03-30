7d18aa8490c6b11fbed9b492a6b0334d22db5bb7
JunHeeCh <apdlsid1@naver.com>
2026-03-30 11:42:59 +0900

    feat(movie): 영화 목록 조회 시 nickname 반환

--- movie-service/src/main/java/com/example/movieservice/domain/repository/CreatorRepository.java
+++ movie-service/src/main/java/com/example/movieservice/domain/repository/CreatorRepository.java
@@ -2,6 +2,7 @@ package com.example.movieservice.domain.repository;
 
 import com.example.movieservice.domain.model.Creator;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -11,4 +12,6 @@ public interface CreatorRepository {
     void save(Creator creator);
 
     Optional<Creator> findById(UUID creatorId);
+
+    List<Creator> findAllByCreatorIdIn(List<UUID> creatorIds);
 }
--- movie-service/src/main/java/com/example/movieservice/infrastructure/persistence/CreatorJpaRepository.java
+++ movie-service/src/main/java/com/example/movieservice/infrastructure/persistence/CreatorJpaRepository.java
@@ -3,7 +3,9 @@ package com.example.movieservice.infrastructure.persistence;
 import com.example.movieservice.domain.model.Creator;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface CreatorJpaRepository extends JpaRepository<Creator, UUID> {
+    List<Creator> findAllByCreatorIdIn(List<UUID> creatorIds);
 }
