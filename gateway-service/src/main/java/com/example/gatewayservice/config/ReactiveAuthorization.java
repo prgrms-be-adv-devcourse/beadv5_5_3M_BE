b3fc7e8274db1febccb625ccdcf2d48dfdf604fd
leeejunu <leeejunu@gmail.com>
2026-03-30 14:58:57 +0900

    feat(gateway): 크리에이터 경로 인가 처리 추가

--- gateway-service/src/main/java/com/example/gatewayservice/config/ReactiveAuthorization.java
+++ gateway-service/src/main/java/com/example/gatewayservice/config/ReactiveAuthorization.java
@@ -16,14 +16,23 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
+import java.util.List;
+
 @Component
 @Slf4j
 public class ReactiveAuthorization implements ReactiveAuthorizationManager<AuthorizationContext> {
 
-    @Value("${apigateway.host:http://localhost:8085}")
-    private String APIGATEWAY_HOST;
+    public static final String USER_AUTHORIZATION_URI = "/api/users/authorization/check";
+    public static final String CREATOR_AUTHORIZATION_URI = "/api/creators/authorization/check";
 
-    public static final String AUTHORIZATION_URI = "/api/users/authorization/check";
+    private final List<String> CREATOR_REQUEST_PATH = List.of("/api/settlements", "/api/movies/creator", "/api/movies/schedules/creator");
+
+    @Value("${user-service.host:http://localhost:8085}")
+    private String USER_SERVICE_HOST;
+    @Value("${creator-service.host:http://localhost:8080}")
+    private String CREATOR_SERVICE_HOST;
+
+
 
     @Override
     public Mono<AuthorizationResult> authorize(Mono<Authentication> authentication, AuthorizationContext context) {
@@ -31,8 +40,10 @@ public class ReactiveAuthorization implements ReactiveAuthorizationManager<Autho
         RequestPath requestPath = request.getPath();
         HttpMethod httpMethod = request.getMethod();
 
-        String baseUrl =
-                APIGATEWAY_HOST + AUTHORIZATION_URI + "?httpMethod=" + httpMethod + "&requestPath=" + requestPath;
+        String baseUrl = USER_SERVICE_HOST + USER_AUTHORIZATION_URI + "?httpMethod=" + httpMethod + "&requestPath=" + requestPath;;
+        if (CREATOR_REQUEST_PATH.stream().anyMatch(path -> requestPath.toString().startsWith(path))) {
+            baseUrl = CREATOR_SERVICE_HOST + CREATOR_AUTHORIZATION_URI + "?httpMethod=" + httpMethod + "&requestPath=" + requestPath;
+        }
         log.info("baseUrl={}", baseUrl);
 
         String userId = request.getHeaders().getFirst("X-User-Id");
@@ -45,9 +56,14 @@ public class ReactiveAuthorization implements ReactiveAuthorizationManager<Autho
 
         boolean granted = false;
         try {
+            String key = "X-User-Id";
+            if (baseUrl.startsWith(CREATOR_SERVICE_HOST)) {
+                key = "X-Creator-Id";
+            }
+            log.info("key = {}, userId = {}", key, userId);
             Mono<Boolean> body = WebClient.create(baseUrl)
                     .get()
-                    .header("X-User-Id", userId)
+                    .header(key, userId)
                     .retrieve().bodyToMono(Boolean.class);
             granted = body.toFuture().get().booleanValue();
             log.info("Security AuthorizationDecision granted={}", granted);
