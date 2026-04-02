1d94a389299d1a2cb10fdc53938c3d1820ed3699
jeongbeomgyu <jeongbeomgyu@github.com>
2026-04-02 16:31:28 +0900

    스케줄 test

--- ticket-service/src/main/java/com/example/ticketservice/infrastructure/scheduling/ReviewAuthorizationScheduler.java
+++ ticket-service/src/main/java/com/example/ticketservice/infrastructure/scheduling/ReviewAuthorizationScheduler.java
@@ -11,7 +11,7 @@ public class ReviewAuthorizationScheduler {
 
     private final ConfirmScheduledTicketsUseCase confirmScheduledTicketsUseCase;
 
-    @Scheduled(cron = "0 */1 * * * *")
+    @Scheduled(cron = "0 * * * * *")
     public void run() {
         confirmScheduledTicketsUseCase.confirm();
     }
