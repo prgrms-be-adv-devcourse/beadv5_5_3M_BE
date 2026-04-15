e9d6f05e4955c2be6ffd18c1028f23eb53066df9
leeejunu <leeejunu@gmail.com>
2026-04-15 15:51:33 +0900

    refactor(user): 패키지 구조 수정

--- user-service/src/main/java/com/example/userservice/exception/InvalidEmailOrPasswordException.java
+++
@@ -1,7 +0,0 @@
-package com.example.userservice.exception;
-
-public class InvalidEmailOrPasswordException extends RuntimeException {
-    public InvalidEmailOrPasswordException() {
-        super("이메일 또는 비밀번호가 올바르지 않습니다.");
-    }
-}
