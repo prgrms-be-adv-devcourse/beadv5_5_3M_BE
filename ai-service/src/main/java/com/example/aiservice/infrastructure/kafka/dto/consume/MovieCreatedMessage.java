c609df8302e149bd0f02012d1f875d5b30def59d
JunHeeCh <apdlsid1@naver.com>
2026-04-19 23:17:52 +0900

    refactor(ai): MovieCreatedMessage / UserCreatedMessage 불필요 필드 제거

--- ai-service/src/main/java/com/example/aiservice/infrastructure/kafka/dto/consume/MovieCreatedMessage.java
+++ ai-service/src/main/java/com/example/aiservice/infrastructure/kafka/dto/consume/MovieCreatedMessage.java
@@ -1,13 +1,8 @@
 package com.example.aiservice.infrastructure.kafka.dto.consume;
 
-import java.util.UUID;
-
 public record MovieCreatedMessage(
         Long movieId,
-        String title,
         String[] category,
-        String imageUrl,
-        UUID creatorId,
         String description
 ) {
 }
