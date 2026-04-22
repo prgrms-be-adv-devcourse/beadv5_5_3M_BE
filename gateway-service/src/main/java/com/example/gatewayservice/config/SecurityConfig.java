0980b7a3cd1f0a93199d3ad6b587b91a69de5aff
leeejunu <leeejunu@gmail.com>
2026-04-22 16:21:57 +0900

    feat(gateway): cors 설정 변경

--- gateway-service/src/main/java/com/example/gatewayservice/config/SecurityConfig.java
+++ gateway-service/src/main/java/com/example/gatewayservice/config/SecurityConfig.java
@@ -42,7 +42,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("https://3m-one.vercel.app", "http://localhost:5173"));
+        config.setAllowedOrigins(List.of("https://3m-cinestream.vercel.app", "http://localhost:5173"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
