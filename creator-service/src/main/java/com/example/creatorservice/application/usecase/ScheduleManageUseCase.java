dc12d4d4ed503c1fc70a3cc06c41cf46e4379ee9
jeongbeomgyu <jeongbeomgyu@github.com>
2026-04-29 12:55:19 +0900

    feat(creator): 특정 날짜 일정 조회로 변경

--- creator-service/src/main/java/com/example/creatorservice/application/usecase/ScheduleManageUseCase.java
+++ creator-service/src/main/java/com/example/creatorservice/application/usecase/ScheduleManageUseCase.java
@@ -16,7 +16,5 @@ public interface ScheduleManageUseCase {
 
     void delete(UUID creatorId, Long scheduleId);
 
-    List<ScheduleResponse> getDraftByDate(UUID creatorId, LocalDate date);
-
-    List<ScheduleResponse> getConfirmedByDate(UUID creatorId, LocalDate date);
+    List<ScheduleResponse> getSchedulesByDate(UUID creatorId, LocalDate date);
 }
