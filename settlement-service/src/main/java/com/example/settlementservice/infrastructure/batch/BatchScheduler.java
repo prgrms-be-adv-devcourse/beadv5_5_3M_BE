f91e1fee79f4be5851111316025e3c2600f380c7
jeongbeomgyu <jeongbeomgyu@github.com>
2026-04-29 17:07:36 +0900

    스케줄 변경

--- settlement-service/src/main/java/com/example/settlementservice/infrastructure/batch/BatchScheduler.java
+++ settlement-service/src/main/java/com/example/settlementservice/infrastructure/batch/BatchScheduler.java
@@ -27,12 +27,12 @@ public class BatchScheduler {
         this.settlementPayoutJob = settlementPayoutJob;
     }
 
-    @Scheduled(cron = "0 12 17 * * *", zone = "Asia/Seoul")
+    @Scheduled(cron = "0 15 17 * * *", zone = "Asia/Seoul")
     public void runConfirmJob() {
         runJob(settlementConfirmJob, "settlementConfirmJob");
     }
 
-    @Scheduled(cron = "0 13 17 * * *", zone = "Asia/Seoul")
+    @Scheduled(cron = "0 16 17 * * *", zone = "Asia/Seoul")
     public void runPayoutJob() {
         runJob(settlementPayoutJob, "settlementPayoutJob");
     }
